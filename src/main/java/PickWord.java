5693b293ac5d9ec28f1f7df7f20d08fa82b0cc1e
Egor18032019 <goro4@mail.ru>
2022-06-23 11:53:21 +0500

    PickWord up

--- src/main/java/PickWord.java
+++ src/main/java/PickWord.java
@@ -16,60 +16,57 @@ public class PickWord {
         Scanner in = new Scanner(System.in);
         String word = in.nextLine();
         String attempt = in.nextLine();
+        in.close();
+//        --- начали проверку--
+        long time = System.currentTimeMillis();
+
         if (word.trim().isEmpty() || attempt.trim().isEmpty()) {
             System.out.println("correct");
         }
+
         char[] wordToArray = word.toCharArray();
         char[] attemptToArray = attempt.toCharArray();
 
         int lengthWord = wordToArray.length;
 
         String[] output = new String[lengthWord];
-        char[] wordToArrayAfterCorrect = new char[lengthWord];
-        char[] attemptToArrayAfterCorrect = new char[lengthWord];
-// пробежали циклом и сравнили каждый знак
+
         for (int i = 0; i < lengthWord; i++) {
             if (wordToArray[i] == attemptToArray[i]) {
                 output[i] = "correct";
-            } else {
-                wordToArrayAfterCorrect[i] = wordToArray[i];
-                attemptToArrayAfterCorrect[i] = attemptToArray[i];
-
+                wordToArray[i] = '*';
+                attemptToArray[i] = '*';
             }
         }
-/*
-+OVE+
-+LEV+
+//              BBA wordToArray
+//              A AA attemptToArray
+//              * O  V ER wordToArray
+//              * L  E AR attemptToArray
 
-correct
-        absent
-    present
-        absent
-correct
- */
-//        System.out.println(wordToArrayAfterCorrect);
-//        System.out.println(attemptToArrayAfterCorrect);
-
-        for (int i = 0; i < lengthWord; i++) {
-            for (int j = 0; j < lengthWord; j++) {
-
-                if (attemptToArrayAfterCorrect[i] != 0) {
-                    if (attemptToArrayAfterCorrect[i] == wordToArrayAfterCorrect[j]) {
-                        output[i] = "present";
-                        wordToArrayAfterCorrect[j]=0;
+//                A *   CBC wordToArray
+//                B * A CA attemptToArray
+        for (int j = 0; j < lengthWord; j++) {
+            if (attemptToArray[j] != '*') {
+                for (int k = 0; k < lengthWord; k++) {
+                    if (wordToArray[k] == '*') continue;
+                    boolean isPresent = attemptToArray[j] == wordToArray[k];
+                    if (isPresent) {
+                        output[j] = "present";
+                        wordToArray[k] = '*';
+                        attemptToArray[j] = '*';
                         break;
 //                        Каждую букву строки S можно использовать не более чем в одном совпадении типа correct или present.
                     }
                 }
-
+            }
+            if (output[j] == null) {
+                output[j] = "absent";
             }
 
-            if (output[i] == null) {
-                output[i] = "absent";
-            }
-            System.out.println(output[i]);
+            System.out.println(output[j]);
         }
+        System.out.println("time - " + (System.currentTimeMillis() - time));
     }
-
-
 }
+
+
