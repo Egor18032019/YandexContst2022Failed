ffe0fd44c2c027c9ebea94b053b7f010f40557f4
Egor18032019 <goro4@mail.ru>
2022-07-19 16:51:05 +0500

    с HashMap на другое

--- src/main/java/Arithmetics.java
+++ src/main/java/Arithmetics.java
@@ -9,39 +9,37 @@ public class Arithmetics {
                 new BufferedReader(new InputStreamReader(System.in));
         String firstLine = reader.readLine();
         int count = Integer.parseInt(firstLine);
-        Map<Integer, Integer> workBook = new HashMap<>();
+        // Если таких прогрессий несколько,
+        // то обрабатывается прогрессия, у которой минимальный идентификатор.
+        Map<Integer, Integer> workBook = new TreeMap<>();
         Map<Integer, Integer> workBookForDiff = new HashMap<>();
-        for (int i = 0; i <count; i++) {
+        for (int i = 0; i < count; i++) {
             String[] stroke = reader.readLine().trim().split(" ");
-//            System.out.println(i);
-//            System.out.println(Arrays.toString(stroke));
-            if (stroke[0].equals("1")) {
-                int key = Integer.parseInt(stroke[3]);
-                int values = Integer.parseInt(stroke[1]);
-                int difference = Integer.parseInt(stroke[2]);
+
+//            if (stroke[0].equals("1")) {
+            if (stroke.length == 4) {
+                Integer key = Integer.valueOf(stroke[3]);
+                Integer values = Integer.valueOf(stroke[1]);
+                Integer difference = Integer.valueOf(stroke[2]);
                 workBook.put(key, values);
                 workBookForDiff.put(key, difference);
                 continue;
             }
-            if (stroke[0].equals("2")) {
-                int keyForRemove = Integer.parseInt(stroke[1]);
+            if (stroke.length == 2) {
+                Integer keyForRemove = Integer.valueOf(stroke[1]);
                 workBook.remove(keyForRemove);
+                workBookForDiff.remove(keyForRemove);
                 continue;
             }
-            if (stroke[0].equals("3")) {
-                // найти и вывести меньший элемент в консоль
-                int min = Collections.min(workBook.values());
-//                System.out.println("min");
-                System.out.println(min);
-                Integer key = getKeytoValue(workBook, min);
-                int newValues = workBook.get(key) + workBookForDiff.get(key);
-                workBook.put(key, newValues);
-                // Если таких прогрессий несколько,
-                // то обрабатывается прогрессия, у которой минимальный идентификатор.
-            } else {
-                System.out.println("Что тут такое ?");
-                System.out.println(Arrays.toString(stroke));
-            }
+//            if (stroke.length == 1) {
+            // остальные случаи выше
+            int min = Collections.min(workBook.values());
+            //предварительно заменив стартовый элемент в прогрессии на следующей в ней.
+            Integer key = getKeytoValue(workBook, min);
+            int newValues = workBook.get(key) + workBookForDiff.get(key);
+            workBook.put(key, newValues);
+            // найти и вывести меньший элемент в консоль
+            System.out.println(min);
         }
         reader.close();
 
@@ -54,4 +52,6 @@ public class Arithmetics {
         }
         return null;
     }
+
+
 }
